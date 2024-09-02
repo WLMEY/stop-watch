161b50d9df5bd2c3344d7eea82b89852fe577452
ENG. Mohammed Hussein kamel <108235027+WLMEY@users.noreply.github.com>
2024-09-02 17:40:08 +0300

    set time

--- stop_watch/src/App.js
+++ stop_watch/src/App.js
@@ -1,9 +1,65 @@
-import './style.css'
-import './App.css';
+import './style.css';
+import { useState, useEffect ,React } from "react";
+const App=()=>{
+  
+  const [t, sett] =useState(0);
+  // const add_t = () => {
+  //     sett(t+5)
+  // }
 
-function App() {
-  return (
+  // useEffect(()=>{
+  // setInterval(add_t,900)
+
+
+
+
+  useEffect(() => {
+    setTimeout(() => {
+      sett((t) => t + 1);
+    }, 1000);
     
+
+
+
+
+
+  return()=>{
+      clearInterval()
+  }
+
+  },)
+
+
+  return (
+   
+    <div className="continer">
+      <div className="continer_watch">
+        <div className="stop_watch">
+          {/* <h1>{t}</h1>
+          <h1>:</h1> */}
+          <h1>{t}</h1>
+          <h1>:</h1>
+          <h1>{Math.floor(t/60)}</h1>
+          <h1>:</h1>
+          <h1>{Math.floor(t/(60*60))}</h1>
+        </div>
+        <div className="btn">
+          <button>start</button>
+          <button>stop</button>
+        </div>
+      </div>
+      <div className="continer_line">
+        <div className="time_line">
+          <div className="line_1"></div>
+          <div className="line_2"></div>
+          <h3>100%</h3>
+          <span>finished</span>
+        </div>
+        <div className="btn">
+        <input placeholder="inter time minute" type="number" />
+        </div>
+      </div>
+    </div>
   );
 }
 
---
+++ stop_watch/src/time_component.js
@@ -0,0 +1,25 @@
+// import { useState, useEffect } from "react";
+
+// const Time = () => {
+
+//     const [t, sett] =useState(0);
+//     const add_t = () => {
+//         sett(t + 1)
+//     }
+
+//     useEffect(()=>{
+//     setInterval(add_t,1)
+//     return()=>{
+//         clearInterval()
+//     }
+
+//     },)
+
+// return()=>{
+//     console.log("is worked")
+// }
+
+
+// }
+// // Time()
+// export default Time;
