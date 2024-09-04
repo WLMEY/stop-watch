25591260c8895c87f6b602cb1cc0a46ba4597061
ENG. Mohammed Hussein kamel <108235027+WLMEY@users.noreply.github.com>
2024-09-04 09:01:23 +0300

    fix errors

--- stop_watch/src/App.js
+++ stop_watch/src/App.js
@@ -6,74 +6,73 @@ import { useState, useEffect, React } from "react";
 
 
 
+
 const App = () => {
 
   const [t, sett] = useState(0);
   const [min, setmin] = useState(0)
   const [hr, sethr] = useState(0)
-  const [total_min , settotal_min]=useState(0)
+  const [total_min, settotal_min] = useState(0)
 
-  // const add_t = () => {
-  //     sett(t+5)
-  // }
-
-  // useEffect(()=>{
-  // setInterval(add_t,900)
-
-
-
-  useEffect(() => {
-    setTimeout(() => {
-      sett((t) => t + 1);
-      if (t == 59) {
-        setmin(min + 1)
-        settotal_min(total_min+1)
-        sett(0)
-      }
-
-      if (min == 59) {
-        sethr(hr + 1)
-        setmin(0)
-      }
-
-    }, 1000);
-
-
-
-    function Timer() {
+  function Work() {
+    useEffect(() => {
+      const time_line_100 = document.getElementById("time_line_100");
       const num = document.getElementById("input").value;
-      const target_hr = Math.floor(num / 60);   // 3.7
-      const target_min = Math.floor((target_hr - (num / 60)) * 60)   // 3- 3.7 =0.7 *60=42
+      setTimeout(() => {
+        sett((t) => t + 1);
+        if (t == 59) {
+          setmin(min + 1)
+          settotal_min(total_min + 1)
+          sett(0)
+        }
 
-      if (target_hr == hr && target_min == min) {
-        <Stop />
-      } else {
-        <Start />
-      }
+        if (min == 59) {
+          sethr(hr + 1)
+          setmin(0)
+        }
+
+      }, 1000);
+      // return () => {
+      //   clearInterval()
+      // }
+
+    });
+  }
+
+  function Timer() {
+    const num = document.getElementById("input").value;
+    const target_hr = Math.floor(num / 60);   // 3.7
+    const target_min = Math.floor((target_hr - (num / 60)) * 60)   // 3- 3.7 =0.7 *60=42
+
+    if (target_hr == hr && target_min == min) {
+      <Stop />
+    } else {
+      <Start />
     }
+  }
+  function time_line() {
+    const time_line_100 = document.getElementById("time_line_100");
+    const num = document.getElementById("input").value;
+    const x = Math.floor((total_min * 100) / num);
+    time_line_100.textContent = `${x}%`
+  }
+  function start_btn() {
+    // const str_btn=document.getElementById("str_btn")
 
-    function time_line(){
-      const time_line_100=document.getElementById("time_line_100");
-      const num = document.getElementById("input").value;
-      const x=Math.floor((total_min*100)/num);
-      time_line_100.textContent=`${x}%`
-    }
-    function start_btn(){
-      // const str_btn=document.getElementById("str_btn")
-      
-    }
-    function stop_btn(){
-      // const stp_btn=document.getElementById("stp_btn")
+    Timer();
+    time_line();
+    Work();
+  }
+  function stop_btn() {
+    // const stp_btn=document.getElementById("stp_btn")
 
-    }
+  }
 
 
 
-    return () => {
-      clearInterval()
-    }
 
-  },)
+
+
 
 
 
@@ -92,8 +91,8 @@ const App = () => {
           <h1>{Math.floor(hr)}</h1>
         </div>
         <div className="btn">
-          <button id='str_btn'>start</button>
-          <button id='stp_btn'>stop</button>
+          <button id='str_btn' onClick={start_btn()}>start</button>
+          <button id='stp_btn' onClick={stop_btn()}>stop</button>
         </div>
       </div>
       <div className="continer_line">
